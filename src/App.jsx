74a3156abbc4156a0ea560867098e5ff1732d69d
ke975 <kevcastle123@gmail.com>
2025-03-24 20:27:13 -0600

    Eniar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import { Footer } from "./components/Footer";
 import sectionImage from "./assets/Section.png"; // Importación correcta
 import sectionImage1 from "./assets/Section1.png";
 import section3 from "./assets/Section3.png"; //
-import marketing from "./assets/Logo1.png";
+import Logo1 from "./assets/Logo1.png";
+
+
 const divStyle = {
   backgroundImage: `url(${sectionImage})`,
   backgroundSize: "cover",
@@ -42,7 +44,7 @@ function App() {
 <p className="">Semadd es una agencia especializada en soluciones digitales integrales, enfocada en ofrecer servicios de marketing, diseño y desarrollo web para empresas que buscan destacar en el mundo digital. Nuestro equipo de expertos trabaja de la mano con cada cliente para crear estrategias de marketing personalizadas, desarrollar sitios web visualmente atractivos y funcionales, y optimizar la presencia online de las marcas a través de campañas de marketing digital efectivas. En Semadd, entendemos que cada empresa tiene sus propias necesidades y objetivos, por lo que nos aseguramos de brindar soluciones a medida que ayuden a nuestros clientes a crecer y conectar con su público objetivo. Con un enfoque innovador y resultados medibles, estamos comprometidos en ser el socio digital que tu negocio necesita para lograr el éxito.</p>
 </div>
 <div className="col-md-6">
-<img className=" img-fluid mt-5" src={marketing} alt="" />
+<img className=" img-fluid mt-5" src={Logo1} alt="" />
 </div>
 
 
