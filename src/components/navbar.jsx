0b718ad0b56756852d20297c5cfa9f70be7a3873
kevcastleunflep <kevin.castillo@unflep.edu.ni>
2025-07-09 20:43:32 -0600

    Reparar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/Logo.png'
  export function Nav() {
 
     const [state, setState] = useState(false)
